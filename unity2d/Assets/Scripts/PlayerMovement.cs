7d084e432ca2ba632c6bed5c9fe7f7a4e2dfee83
Berk <berkgguzel@gmail.com>
2025-05-15 17:13:24 +0300

    animasyonlar, hareketler eklendi
    Co-Authored-By: Berkay Enes Yorgalı <121587069+BerkayYorgali@users.noreply.github.com>

---
+++ unity2d/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rgb;
+    Vector3 velocity;
+    public Animator animator;
+
+    float speedAmount = 5f;
+    float jumpAmount = 7f;
+
+
+    void Start()
+    {
+     rgb = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = new Vector3(Input.GetAxis("Horizontal"), 0f);
+        transform.position += velocity * speedAmount * Time.deltaTime;
+        animator.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+
+        if (Input.GetButtonDown("Jump") && Mathf.Approximately(rgb.linearVelocity.y,0))
+        {
+            rgb.AddForce(Vector3.up * jumpAmount, ForceMode2D.Impulse);
+            animator.SetBool("isJumping", true);
+        }
+
+        if(animator.GetBool("isJumping") && Mathf.Approximately(rgb.linearVelocity.y, 0))
+        {
+            animator.SetBool("isJumping", false);
+
+        }
+
+        if (Input.GetAxisRaw("Horizontal") == -1)
+        {
+            transform.rotation = Quaternion.Euler(0f,180f,0f);
+        }
+        else if(Input.GetAxisRaw("Horizontal") == 1)
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+    }
+}
