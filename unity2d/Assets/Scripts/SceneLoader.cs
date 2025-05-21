eba2db6fa88cc76dbe9f5c9ddd195d83dd449270
Tareq Alhammoodi <44919941+tareqalhammoodi@users.noreply.github.com>
2025-05-21 17:21:46 +0300

    scenes fix

--- unity2d/Assets/Scripts/SceneLoader.cs
+++ unity2d/Assets/Scripts/SceneLoader.cs
@@ -3,6 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public void ReturnMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
     public void LoadLevel2()
     {
         SceneManager.LoadScene("Level2Scene");
